15b05ff22ab61f4379c4579666bec7ec41384736
milbrathdrew <milbrathdrew@gmail.com>
2024-02-03 13:50:37 -0700

    added auth context from turing example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 // App.js
+
+/*Consuming the AuthContext: Consuming the Authentication Context is straightforward. 
+Wrap your entire application with the AuthProvider and use the useAuth hook in any component that needs access to authentication data.
+*/
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
+import { AuthProvider } from './contexts/AuthContext';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/login" element={<LoginForm />} />
-        <Route exact path="/registration" element={<RegistrationForm />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={<LoginForm />} />
+          <Route exact path="/registration" element={<RegistrationForm />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
